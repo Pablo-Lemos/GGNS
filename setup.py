f9cdff8d8c94a45ba7232f0e2a5e36868512eaaf
Pablo Lemos <plemos91@gmail.com>
2024-01-23 14:50:40 -0500

    Updated setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,11 @@
 from setuptools import setup
 
 setup(name='gradNS',
-      version='0.1',
+      version='0.1.1',
       description='Differentiable Nested Sampler',
+      url='https://github.com/Pablo-Lemos/torchNS.git',
+      author='Pablo Lemos',
+      author_email='pablo.lemos@umontreal.ca',
       license='MIT',
       packages=['gradNS'],
       zip_safe=False)
